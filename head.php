028df603b443c93a33019d9f26c1c63a40cac7cb
João Barroso <trabalho.joaobarroso@gmail.com>
2025-05-13 00:34:10 +0100

    feat: add fade-in animation to elements and update login form structure

--- head.php
+++ head.php
@@ -13,6 +13,24 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
+      <style>
+        /* Add fade-in animation */
+        @keyframes fadeIn {
+            from {
+                opacity: 0;
+                transform: translateY(-20px);
+            }
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+
+        .fade-in {
+            animation: fadeIn 1s ease-in-out;
+        }
+    </style>
+    
 <script src="https://cdn.jsdelivr.net/npm/tinymce@6/tinymce.min.js"></script>
 <script>
     tinymce.init({
--- login.php
+++ login.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-pt">
 <?php include('head.php'); ?>
+
 <body>
 
   <!-- Conteúdo da página -->
-    <section class="container w-100 h-100 justify-content-center align-content-center">
-    
-        <div class="login-form shadow-lg p-3 mb-5 bg-white rounded w-100 mx-auto flex-column">
-            <h2 class="fw-bold mb-4 mt-3">Login</h2>
-            <form action="logged.php" method="POST">
+    <section class="w-100 h-100"
+    style="background: linear-gradient(to right,rgb(34, 76, 95), rgb(85, 160, 191));">
+        <div class="container d-flex justify-content-center align-items-center h-100">
+                <div class="col-4 p-3 bg-white rounded flex-column justify-content-center align-items-center fade-in">
+                    <h2 class="fw-bold mb-4 mt-4 text-center">Bem Vindo</h2>
+                    <form action="logged.php" method="POST">
 
-            <!-- Campo de E-mail -->
-            <div class="input-group mb-4 w-75 mx-auto d-flex justify-content-center">
+                    <!-- Campo de E-mail -->
+                    <div class="input-group mb-4 w-100  d-flex justify-content-center flex-column">
+                        <label for="email" class="mb-1">Email:</label>
+                        <input type="email" class="form-control w-100 rounded" id="email" name="email" placeholder="example@domain.com">
+                    </div>
 
-                <span class="input-group-text bg-light"><i class="bi bi-person"></i></span>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Digite o seu e-mail..." required style="background-color: #f0f0f0; border: none;">
-            </div>
+                        <!-- Campo de Palavra-Passe -->
+                        <div class="input-group mb-4 w-100 d-flex justify-content-center flex-column">
+                            <label for="password" class="mb-1">Palavra-Passe:</label>
+                            <input type="password" class="form-control w-100 rounded" id="password" name="password" placeholder="********" >
+                        </div>
 
-                <!-- Campo de Senha -->
-                <div class="input-group mb-4 w-75 mx-auto d-flex justify-content-center">
-
-                    <span class="input-group-text bg-light"><i class="bi bi-lock"></i></span>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Digite a sua senha..." required style="background-color: #f0f0f0; border: none;">
+                        <!-- Botão Para enviar Formulário -->
+                        <button type="submit" class="btn btn-primary mb-4 w-75 mx-auto d-flex justify-content-center">Iniciar Sessão</button>
+                    </form>
                 </div>
-
-                <!-- Botão Para enviar Formulário -->
-                <button type="submit" class="btn btn-primary mb-4 w-75 mx-auto d-flex justify-content-center">Entrar</button>
-            </form>
         </div>
 </section>
     <!-- Scripts do Bootstrap e JQuery -->
